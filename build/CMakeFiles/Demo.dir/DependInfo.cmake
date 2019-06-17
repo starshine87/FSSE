dc0d0571a3e5360fc5c91e8518e1fc2b11f14e7d
starshine87 <starshine87@sjtu.edu.cn>
2019-06-17 17:21:48 +0800

    demo

---
+++ build/CMakeFiles/Demo.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/star/OneDrive/CODES/fsse/src/client.cpp" "/Users/star/OneDrive/CODES/fsse/build/CMakeFiles/Demo.dir/src/client.cpp.o"
+  "/Users/star/OneDrive/CODES/fsse/src/main.cpp" "/Users/star/OneDrive/CODES/fsse/build/CMakeFiles/Demo.dir/src/main.cpp.o"
+  "/Users/star/OneDrive/CODES/fsse/src/util.cpp" "/Users/star/OneDrive/CODES/fsse/build/CMakeFiles/Demo.dir/src/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/opt/openssl/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Demo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Demo.dir/src/client.cpp.o"
+  "CMakeFiles/Demo.dir/src/main.cpp.o"
+  "CMakeFiles/Demo.dir/src/util.cpp.o"
+  "Demo"
+  "Demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
